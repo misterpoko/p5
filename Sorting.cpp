6a0c4c9d3a9ab2e5d92a4417c68c44868e2d2cc2
Nathaniel <nbm54197@uga.edu>
2021-11-20 11:06:19 -0500

    selection sort

--- Sorting.cpp
+++ Sorting.cpp
@@ -5,8 +5,27 @@ using namespace std;
 
 long comparisons = 0;
 
-void SS(int* array, int numElements)
+void SS(int* array, int size)
 {
+	int low = array[0];
+	int swapper;
+	int tracker;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = size-1; j > i; j--)
+		{
+			if (array[j] < low)
+			{
+				tracker = j;
+				low = array[j];
+			} // if
+		} // for
+		swapper = array[i];
+		array[i] = low;
+		array[tracker] = swapper;
+	} // for
+	for (int i = 0; i < size; i++)
+		cout << array[i] << " " << endl;
 } // ss
 
 void MS()
