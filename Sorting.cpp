bab8334b2d3d4198945e5baa950868bd2f5041ff
Nathaniel <nbm54197@uga.edu>
2021-11-23 16:21:48 -0500

    more comments yay

--- Sorting.cpp
+++ Sorting.cpp
@@ -121,7 +121,7 @@ void Sorting::MS(int array[], int const begin, int const end)
 } // MS
 
 /**
- * This is the sorting part of heapsort. It turns the data into a heap and then promptly sorts it.
+ * This is the sorting part of heapsort. It takes a heap and promptly sorts it.
  * The heap is still array based.
  * @param arr the array to be sorted.
  * @param n index of smallest element
@@ -160,110 +160,156 @@ void Sorting::heapify(int arr[], int n, int i)
 	} // if
 } // heapify 
 
+/**
+ * This builds the initial heap. Then starts to call heapify to sort the heap.
+ * @param arr the array to be sorted.
+ * @param n the number of elements in the array.
+ */
 void Sorting::HS(int* arr, int n) //Heap
 {
-   // Build heap (rearrange array)
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
+	// Build heap (rearrange array)
+	for (int i = n / 2 - 1; i >= 0; i--)
+        	heapify(arr, n, i);
  
-    // One by one extract an element from heap
-    for (int i = n - 1; i > 0; i--) {
-        // Move current root to end
-        swap(arr[0], arr[i]);
- 
-    // call max heapify on the reduced heap
-    heapify(arr, i, 0);
-    }
+	// One by one extract an element from heap
+	for (int i = n - 1; i > 0; i--)
+	{
+		// Move current root to end
+        	swap(arr[0], arr[i]);
+
+		// call max heapify on the reduced heap
+		heapify(arr, i, 0);
+	} // for
 } // HS
 
-
+/**
+ * This is the sorting part of quicksort. It makes the pivot the first element.
+ * @param arr the array to be sorted
+ * @param start the start of the part of the array to be sorted
+ * @param last the end of the part of the array to be sorted
+ * @return its the next splitting point of the array
+ */
 int Sorting::partition1(int *arr,int start,int last)
 {
-int pivot=arr[start],p1=start+1,i,temp;
-for(i=start+1;i<=last;i++)
-{
-comparisons++;
-if(arr[i]<pivot)
-{
-if(i!=p1)
-{  
-temp=arr[p1];
-arr[p1]=arr[i];
-arr[i]=temp;
-}   
-p1++;
-}
-}
-arr[start]=arr[p1-1];
-arr[p1-1]=pivot;
-return p1-1;
-}
+	int pivot=arr[start],p1=start+1,i,temp;
+	for(i=start+1;i<=last;i++)
+	{
+		comparisons++;
+		if(arr[i]<pivot)
+		{
+			if(i!=p1)
+			{  
+				temp=arr[p1];
+				arr[p1]=arr[i];
+				arr[i]=temp;
+			} // if   
+			p1++;
+		} // if
+	} // for
+	arr[start]=arr[p1-1];
+	arr[p1-1]=pivot;
+	return p1-1;
+} // partition1
 
-
+/**
+ * This is the recursive part of quicksort. It takes a pivot and in this case the first element of the section that 
+ * it sorts.
+ * @param arr the array to be sorted
+ * @param start the start index of the part of the array to be sorted
+ * @param last the last index of the part of the array to be sorted
+ */
 void Sorting::QS1(int* arr, int start,int last) //QuickSort at first
 {
-int p1;
-if(start<last)
-{
-p1=partition1(arr,start,last);
-QS1(arr,start,p1-1);
-QS1(arr,p1+1,last);
-}
+	int p1;
+	if(start<last)
+	{
+		p1=partition1(arr,start,last);
+		QS1(arr,start,p1-1);
+		QS1(arr,p1+1,last);
+	} // if
 } // QS1
 
+/**
+ * This method swaps two elements in an array.
+ * @param a the first element
+ * @param b the last element
+ */
 void swap(int* a, int* b) 
 { 
-int t = *a; 
-*a = *b; 
-*b = t; 
-} 
+	int t = *a; 
+	*a = *b; 
+	*b = t; 
+} // swap
 
+/**
+ * This is the partition of a last element array. It also is the sorting part of 
+ * the quicksort algorithm.
+ * @param arr the array to be sorted
+ * @param low the lowest index of the section of the array to be sorted
+ * @param high the highest index of the section of the array to be sorted
+ * @return int the next splitting point of the array.
+ */
 int Sorting::partition(int arr[], int low, int high)
 {
-// pivot
-int pivot = arr[high];
+	// pivot
+	int pivot = arr[high];
 
-// Index of smaller element
-int i = (low - 1);
+	// Index of smaller element
+	int i = (low - 1);
 
-for (int j = low; j <= high - 1; j++)
-{
-// If current element is smaller than or equal to pivot
-if (arr[j] <= pivot) {
+	for (int j = low; j <= high - 1; j++)
+	{
+		// If current element is smaller than or equal to pivot
+		if (arr[j] <= pivot)
+		{
+			// increment index of smaller element
+			i++;
+			swap(arr[i], arr[j]);
+		} // if
+		comparisons++;
+	} // for
+	swap(arr[i + 1], arr[high]);
+	return (i + 1);
+} // partition
 
-// increment index of smaller element
-i++;
-swap(arr[i], arr[j]);
-}
-comparisons++;
-}
-swap(arr[i + 1], arr[high]);
-return (i + 1);
-}
-
-// Generates Random Pivot, swaps pivot with end element and calls the partition function
+/**
+ * This method takes a section of an array and swaps a random element out with the highest element index in that section
+ * It is used in conjunction with partition to create a random partition
+ * @param arr the array to be sorted
+ * @param low the lowest index of the section of the array
+ * @param high the highest index of the section of the array
+ * @return the next splitting point of the array using the partition algorithm
+ */
 int Sorting::partition_r(int arr[], int low, int high)
 {
-// Generate a random number in between low .. high
-srand(time(NULL));
-int random = low + rand() % (high - low);
+	// Generate a random number in between low .. high
+	srand(time(NULL));
+	int random = low + rand() % (high - low);
 
-// Swap A[random] with A[high]
-swap(arr[random], arr[high]);
+	// Swap A[random] with A[high]
+	swap(arr[random], arr[high]);
 
-return partition(arr, low, high);
-}
+	return partition(arr, low, high);
+} // partition_r
 
+/**
+ * This is the random quicksort. It takes uses the partition_r method to find its pivots and uses the partition method to sort
+ * the elements
+ * @param arr the array to be sorted
+ * @param low the lowest index of the array to be sorted
+ * @param high the highest index of the array to be sorted
+ * @return number of comparisons made throughout the method
+ */
 int Sorting::QS2(int* arr, int low,int high) //QuickSort at random
 {
-if (low < high) 
-{ 
-// pi is partitioning index, arr[p] is now at right place 
-int pi = partition_r(arr, low, high); 
+	if (low < high) 
+	{ 
+		// pi is partitioning index, arr[p] is now at right place 
+		int pi = partition_r(arr, low, high); 
 
-// Separately sort elements before partition and after partition 
-QS2(arr, low, pi - 1); 
-QS2(arr, pi + 1, high); 
-} 
-return comparisons;
+		// Separately sort elements before partition and after partition 
+		QS2(arr, low, pi - 1); 
+		QS2(arr, pi + 1, high); 
+	} // if 
+	return comparisons;
 } // QS2
